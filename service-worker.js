868a1445b2ab835e8ac96418294fe3d9ac978a33
fipavtest-bit <fipavtest@gmail.com>
2026-02-26 15:17:16 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,19 @@
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("print-calculator").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
